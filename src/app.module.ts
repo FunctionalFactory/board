01d6aab9648f0f4617254377a1b71117e94f78ad
handymansodo <handymansodo@gmail.com>
2024-08-15 23:33:27 +0900

    [feat]게시판 개발 초기준비

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    BoardsModule,
   ],
   controllers: [],
   providers: [],
