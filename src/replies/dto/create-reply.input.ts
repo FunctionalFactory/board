34c49368c7630e1f901f8a50bd7f0cb50f7f2424
handymansodo <handymansodo@gmail.com>
2024-08-16 19:01:38 +0900

    [feat] 댓글,대댓글 조회 및 class-validator class-transformer 라이브러리 추가 및 댓글 서비스 매서드 리팩토링

--- src/replies/dto/create-reply.input.ts
+++ src/replies/dto/create-reply.input.ts
@@ -1,6 +1,16 @@
+import { IsString, IsNumber, IsOptional } from 'class-validator';
+
 export class CreateReplyInput {
+  @IsString()
   writer: string;
+
+  @IsString()
   contents: string;
+
+  @IsNumber()
   boardId: number;
+
+  @IsOptional()
+  @IsNumber()
   parentReplyId?: number;
 }
--- src/replies/interfaces/replies.service.interface.ts
+++ src/replies/interfaces/replies.service.interface.ts
@@ -1,5 +1,9 @@
 import { CreateReplyInput } from '../dto/create-reply.input';
 
-export class IRepliesServiceCreate {
+export interface IRepliesServiceCreate {
   createReplyInput: CreateReplyInput;
+}
+
+export interface IRepliesServiceFindReplies {
+  boardId: number;
 }
