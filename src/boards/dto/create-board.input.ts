c187eff90833826d9cb749eed48373a3bb1eb19a
handymansodo <handymansodo@gmail.com>
2024-08-16 19:20:52 +0900

    [feat]환경변수 처리 및 class-validator 추가

--- src/boards/dto/create-board.input.ts
+++ src/boards/dto/create-board.input.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class CreateBoardInput {
+  @IsString()
   writer: string;
+
+  @IsString()
   title: string;
+
+  @IsString()
   contents: string;
 }
